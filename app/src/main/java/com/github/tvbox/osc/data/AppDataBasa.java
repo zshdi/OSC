a3811d97b2ee3840538e962aa6616a731770ccb2
zshdi <64761918+zshdi@users.noreply.github.com>
2022-07-03 13:37:21 +0800

    Update AppDataBasa.java

--- app/src/main/java/com/github/tvbox/osc/data/AppDataBasa.java
+++ app/src/main/java/com/github/tvbox/osc/data/AppDataBasa.java
@@ -1,1 +1,2 @@
-https://github.com/zshdi/OSC/blob/main/app/src/main/java/com/github/tvbox/osc/data/AppDataBas1.jpg
+###
+https://github.com/zshdi/OSC/blob/main/app/src/main/java/com/github/tvbox/osc/data/AppDataBas1.java
